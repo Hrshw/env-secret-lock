07778fba25fdcb43f55730da392f77c967d74a4c
Rahul Singh Shekhawat <rahulsinghpilani7@gmail.com>
2026-05-29 16:50:37 +0530

    fix: remove terminal autofocus to prevent autoscroll, and add test files

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("\n==================================================");
+console.log("🔒  SECRET-LOCK RUNTIME INJECTION ACTIVE  🔒");
+console.log("==================================================");
+console.log(`Port Variable:      \x1b[32m${process.env.PORT}\x1b[0m`);
+console.log(`API Key Variable:  \x1b[32m${process.env.API_KEY}\x1b[0m`);
+console.log(`Database URL:      \x1b[32m${process.env.DATABASE_URL}\x1b[0m`);
+console.log("==================================================\n");
